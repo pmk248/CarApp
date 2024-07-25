06ef4e45e27357bafb1c935ebc1412a92236f3c5
pmk248 <pinchaskimel@gmail.com>
2024-07-25 17:14:41 +0300

    Add project files.

---
+++ CarApp/Controllers/CarController.cs
@@ -0,0 +1,14 @@
+﻿using CarApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarApp.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Model(string name, string year, string background)
+        {
+            Car car = new Car { CarModel = name ?? "Unknown", Year = year ?? "Unknown", Background = background ?? "white"};
+            return View(car);
+        }
+    }
+}
---
+++ CarApp/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CarApp.Models
+{
+    public class Car
+    {
+        public string CarModel { get; set; }
+        public string Year { get; set; }
+        public string Background { get; set; }
+    }
+}
